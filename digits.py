a070a30164668873b631cbdef4faa841de748c54
Victor Hall <46335285+VHall1@users.noreply.github.com>
2019-11-19 20:33:51 -0300

    Update digits.py

--- digits.py
+++ digits.py
@@ -1,39 +1,51 @@
-#### Import datasets and some other important stuff ####
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
 from sklearn import svm, neighbors, ensemble
 
-#### Initialize Digits ####
 digits = load_digits()
-expected = digits.target[1::2] #Expected Result (Target)
+expected = digits.target[1::2]
 
-#### SVC ####
-clf = svm.SVC(gamma=0.001, C=100.) 
+clf = svm.SVC(gamma=0.001, C=100.)
 clf.fit(digits.data[::2], digits.target[::2])
 
 results1 = clf.predict(digits.data[1::2])
 score1 = clf.score(digits.data[1::2], digits.target[1::2])
 
-#### KNeighbors ####
+##################################################################
+
 clf2 = neighbors.KNeighborsClassifier()
 clf2.fit(digits.data[::2], digits.target[::2])
 
 results2 = clf2.predict(digits.data[1::2])
 score2 = clf2.score(digits.data[1::2], digits.target[1::2])
 
-#### Ensemble ####
+##################################################################
+
 clf3 = ensemble.RandomForestClassifier()
 clf3.fit(digits.data[::2], digits.target[::2])
 
 results3 = clf3.predict(digits.data[1::2])
 score3 = clf3.score(digits.data[1::2], digits.target[1::2])
 
-#### Plotting Results ####
-names = ["SVC", "KNeighbors", "Ensemble"] #X Labels
-values = [score1, score2, score3] #Actual Values
+##################################################################
 
-print(values) #Debug print. Prints each classifier's score
+names = ["SVC", "KNeighbors", "Ensemble"]
+values = [score1 * 100 , score2 * 100 , score3 * 100]
+
+print(values)
 
 plt.bar(names, values)
+
+def addLabelOnTop(barras):
+    k = -1.25
+    for barra in barras:
+        k = k + 1
+        plt.annotate("{0:.2f}".format(barra) + "%",
+            xy=(0.1+k, 100), xycoords='data', textcoords='data')
+        
+
 plt.title("Comparando Algoritmos")
+
+addLabelOnTop(values)
+
 plt.show()
